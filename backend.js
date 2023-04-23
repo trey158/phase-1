97c2f4a6656131a1864b3205a951895ce1f780c1
trey158 <lycanroc7777@gmail.com>
2023-04-22 20:21:18 -0500

    installed MongoDB driver
    installed MongoDB driver and HOPEFULLY converted current project to use MongoDB instead of saving tickets to a JSON file.

--- backend.js
+++ backend.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const { MongoClient, ObjectID } = require('mongodb');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
@@ -9,35 +9,46 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const ticketsFile = './tickets.json';
+const uri = 'mongodb+srv://admin:CMPS415@cluster0.nttgrun.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const dbName = 'ticketsdb';
+const collectionName = 'tickets';
+
+// Connect to the database
+client.connect((err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log('Connected to MongoDB');
+});
 
 // Endpoint to get all tickets
-app.get('/rest/list', (req, res) => {
-  const tickets = JSON.parse(fs.readFileSync(ticketsFile));
+app.get('/rest/list', async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+  const tickets = await collection.find().toArray();
   res.send(tickets);
 });
 
 // Endpoint to get a single ticket by ID
-app.get('/rest/ticket/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  if (isNaN(id)) {
-    return res.status(400).send('Invalid ID');
-  }
-  
-  const tickets = JSON.parse(fs.readFileSync(ticketsFile));
-  const ticket = tickets.find((t) => t.id === parseInt(req.params.id));
+app.get('/rest/ticket/:id', async (req, res) => {
+  const id = req.params.id;
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+  const ticket = await collection.findOne({ _id: ObjectID(id) });
   if (!ticket) {
     return res.status(404).send('Ticket not found');
   }
-  
   res.send(ticket);
 });
 
 // Endpoint to create a new ticket
-app.post('/rest/ticket', (req, res) => {
-  const tickets = JSON.parse(fs.readFileSync(ticketsFile));
+app.post('/rest/ticket', async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
   const newTicket = {
-    id: uuidv4(),
+    _id: new ObjectID(),
     created_at: new Date(),
     updated_at: new Date(),
     type: req.body.type,
@@ -51,11 +62,36 @@ app.post('/rest/ticket', (req, res) => {
     follower_ids: req.body.follower_ids,
     tags: req.body.tags,
   };
-  tickets.push(newTicket);
-  fs.writeFileSync(ticketsFile, JSON.stringify(tickets, null, 2));
+  const result = await collection.insertOne(newTicket);
   res.send(newTicket);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+// Endpoint to delete a ticket by ID
+app.delete('/rest/ticket/:id', async (req, res) => {
+  const id = req.params.id;
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+  const result = await collection.deleteOne({ _id: ObjectID(id) });
+  if (result.deletedCount === 0) {
+    return res.status(404).send('Ticket not found');
+  }
+  res.send('Ticket deleted successfully');
 });
+
+// Endpoint to update a ticket by ID
+app.put('/rest/ticket/:id', async (req, res) => {
+  const id = req.params.id;
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+  const ticket = await collection.findOne({ _id: ObjectID(id) });
+  if (!ticket) {
+    return res.status(404).send('Ticket not found');
+  }
+  const updatedTicket = {
+    _id: ObjectID(id),
+    created_at: ticket.created_at,
+    updated_at: new Date(),
+    type: req.body.type || ticket.type,
+    subject: req.body.subject || ticket.subject,
+    description: req.body.description || ticket.description,
+    
